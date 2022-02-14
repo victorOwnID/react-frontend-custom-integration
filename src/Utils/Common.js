15ff21c62de1bed99d54b063fb60829e3c364d55
Victor Navarro <victor@ownid.com>
2022-02-14 09:26:18 +0100

    small fix

--- src/Utils/Common.js
+++ src/Utils/Common.js
@@ -7,13 +7,17 @@ export const getUser = () => {
 
 export const parseJWT = (token) => {
 
+  if(token !== 'undefined'){
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
-
+  
     return JSON.parse(jsonPayload);
+  }else{
+    return "";
+  }
 }
 
 // return the token from the session storage
